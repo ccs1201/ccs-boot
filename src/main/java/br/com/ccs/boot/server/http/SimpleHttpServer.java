e0e1c8380936bdf87d892cf256836d439a088733
Cleber.Ssouza <ccs1201@gmail.com>
2025-04-28 15:46:51 -0300

    #43 - Configurado um ThreadPoolExecutor para SimpleHttpServer

--- src/main/java/br/com/ccs/boot/server/http/SimpleHttpServer.java
+++ src/main/java/br/com/ccs/boot/server/http/SimpleHttpServer.java
@@ -10,7 +10,9 @@ import jakarta.inject.Singleton;
 import org.slf4j.Logger;
 
 import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @Singleton
 public class SimpleHttpServer {
@@ -18,6 +20,7 @@ public class SimpleHttpServer {
     private final Logger log;
     private HttpServer server;
     private final HandlerDispatcher handlerDispatcher;
+    private final ThreadPoolExecutor executor;
 
     @PreDestroy
     public void destroy() {
@@ -28,6 +31,13 @@ public class SimpleHttpServer {
     public SimpleHttpServer(HandlerDispatcher handlerDispatcher, Logger log) {
         this.handlerDispatcher = handlerDispatcher;
         this.log = log;
+        executor = new ThreadPoolExecutor(
+                10,
+                200,
+                60L,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(200),
+                Thread.ofVirtual().factory());
     }
 
     public void start(ServerConfig config) throws Exception {
@@ -44,14 +54,24 @@ public class SimpleHttpServer {
 
         server = HttpServer.create(new InetSocketAddress(config.port()), 0);
         server.createContext(config.contextPath(), handlerDispatcher);
-        server.setExecutor(Executors.newFixedThreadPool(200, Thread.ofVirtual().factory()));
+        server.setExecutor(executor);
     }
 
     public void stop() {
         log.info("Stopping HTTP server");
-        if (server != null) {
-            server.stop(0);
-            log.info("Server stopped");
+        var executor = (ThreadPoolExecutor) server.getExecutor();
+        if (executor != null) {
+            try {
+                // Aguarda até 30 segundos para finalização das threads
+                if (!executor.awaitTermination(30, TimeUnit.SECONDS)) {
+                    executor.shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                executor.shutdownNow();
+                Thread.currentThread().interrupt();
+            }
         }
+        server.stop(0);
+        log.info("Server stopped");
     }
 }
